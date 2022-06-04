24cef7a3af977c7b9ed1f80932aa4ece93685347
Shouwang Church <shwchurch3@gmail.com>
2022-06-04 20:40:00 +0000

    [Bulk]  - Added wp-content@2 files

--- wp-content/uploads/pwaforwp/pwa-amp-sw.js
+++ wp-content/uploads/pwaforwp/pwa-amp-sw.js
@@ -3,11 +3,11 @@ const CACHE_VERSION = '1.7.34.2';
 const BASE_CACHE_FILES = [
     'https://t5.shwchurch.org/wp-content/uploads/2021/06/439c8112e53379d73724e80c93eccd0c.png',
 'https://t5.shwchurch.org/wp-content/uploads/2019/11/podcast-sw.jpg',
+'https://t5.shwchurch.org/2022/06/05/beijingshouwangjiaohuizhuribaogaoshixiang2022nian6yue5ri/amp/',
+'https://t5.shwchurch.org/2022/06/05/beijingshouwangjiaohui2022nian6yue5rizhurijingbaichengxu/amp/',
+'https://t5.shwchurch.org/2022/06/05/nimenzheyingzhejingxiangde2022nian6yue5rizhurijiangzhang-yuanlingmushi/amp/',
 'https://t5.shwchurch.org/2022/05/27/beijingshouwangjiaohuizhuribaogaoshixiang2022nian5yue29ri/amp/',
 'https://t5.shwchurch.org/2022/05/27/beijingshouwangjiaohui2022nian5yue29rizhurijingbaichengxu/amp/',
-'https://t5.shwchurch.org/2022/05/27/tademianmaohaoxiangtianshidemianmao2022nian5yue29rizhurijiangzhang-xiaofengmushi/amp/',
-'https://t5.shwchurch.org/2022/05/21/beijingshouwangjiaohuizhuribaogaoshixiang2022nian5yue22ri/amp/',
-'https://t5.shwchurch.org/2022/05/21/beijingshouwangjiaohui2022nian5yue22rizhurijingbaichengxu/amp/',
 
 ];
 
--- wp-content/uploads/pwaforwp/pwa-sw.js
+++ wp-content/uploads/pwaforwp/pwa-sw.js
@@ -3,11 +3,11 @@ const CACHE_VERSION = '1.7.34.2';
 const BASE_CACHE_FILES = [
     'https://t5.shwchurch.org/wp-content/uploads/2021/06/439c8112e53379d73724e80c93eccd0c.png',
 'https://t5.shwchurch.org/wp-content/uploads/2019/11/podcast-sw.jpg',
+'https://t5.shwchurch.org/2022/06/05/beijingshouwangjiaohuizhuribaogaoshixiang2022nian6yue5ri/',
+'https://t5.shwchurch.org/2022/06/05/beijingshouwangjiaohui2022nian6yue5rizhurijingbaichengxu/',
+'https://t5.shwchurch.org/2022/06/05/nimenzheyingzhejingxiangde2022nian6yue5rizhurijiangzhang-yuanlingmushi/',
 'https://t5.shwchurch.org/2022/05/27/beijingshouwangjiaohuizhuribaogaoshixiang2022nian5yue29ri/',
 'https://t5.shwchurch.org/2022/05/27/beijingshouwangjiaohui2022nian5yue29rizhurijingbaichengxu/',
-'https://t5.shwchurch.org/2022/05/27/tademianmaohaoxiangtianshidemianmao2022nian5yue29rizhurijiangzhang-xiaofengmushi/',
-'https://t5.shwchurch.org/2022/05/21/beijingshouwangjiaohuizhuribaogaoshixiang2022nian5yue22ri/',
-'https://t5.shwchurch.org/2022/05/21/beijingshouwangjiaohui2022nian5yue22rizhurijingbaichengxu/',
 
 ];
 
