cd8d6b54363c9d0b4256eef360cb3ecdf074c04c
Shouwang Church <shwchurch3@gmail.com>
2022-11-11 20:42:32 +0000

    [Bulk]  - Added wp-content@2 files

--- wp-content/uploads/pwaforwp/pwa-sw.js
+++ wp-content/uploads/pwaforwp/pwa-sw.js
@@ -3,11 +3,11 @@ const CACHE_VERSION = '1.7.34.2';
 const BASE_CACHE_FILES = [
     'https://t5.shwchurch.org/wp-content/uploads/2021/06/439c8112e53379d73724e80c93eccd0c.png',
 'https://t5.shwchurch.org/wp-content/uploads/2019/11/podcast-sw.jpg',
+'https://t5.shwchurch.org/2022/11/11/beijingshouwangjiaohuizhuribaogaoshixiang2022nian11yue13ri/',
+'https://t5.shwchurch.org/2022/11/11/beijingshouwangjiaohui2022nian11yue13rizhurijingbaichengxu/',
+'https://t5.shwchurch.org/2022/11/11/daitatongqu2022nian11yue13rizhurijiangzhang-xiaobaimushi/',
 'https://t5.shwchurch.org/2022/11/05/beijingshouwangjiaohuizhuribaogaoshixiang2022nian11yue6ri/',
 'https://t5.shwchurch.org/2022/11/05/beijingshouwangjiaohui2022nian11yue6rizhurijingbaichengxu/',
-'https://t5.shwchurch.org/2022/11/05/errenqilezhenglun2022nian11yue6rizhurijiangzhang-xiaofengmushi/',
-'https://t5.shwchurch.org/2022/10/30/beijingshouwangjiaohuizhuribaogaoshixiang2022nian10yue30ri/',
-'https://t5.shwchurch.org/2022/10/30/beijingshouwangjiaohui2022nian10yue30rizhurijingbaichengxu/',
 
 ];
 
