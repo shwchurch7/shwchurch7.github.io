750732d2282145eb397ff9066e5a5a83f7b98a69
Shouwang Church <shwchurch3@gmail.com>
2023-07-01 19:54:23 +0000

    [Bulk]  - Added wp-content@2 files

--- wp-content/uploads/pwaforwp/pwa-sw.js
+++ wp-content/uploads/pwaforwp/pwa-sw.js
@@ -3,11 +3,11 @@ const CACHE_VERSION = '1.7.34.2';
 const BASE_CACHE_FILES = [
     'https://t5.shwchurch.org/wp-content/uploads/2021/06/439c8112e53379d73724e80c93eccd0c.png',
 'https://t5.shwchurch.org/wp-content/uploads/2019/11/podcast-sw.jpg',
+'https://t5.shwchurch.org/2023/07/02/beijingshouwangjiaohuizhuribaogaoshixiang2023nian7yue2ri/',
+'https://t5.shwchurch.org/2023/07/02/beijingshouwangjiaohui2023nian7yue2rizhurijingbaichengxu/',
+'https://t5.shwchurch.org/2023/07/02/fengxianliweiren2023nian7yue2rizhurijiangzhang-yuanlingmushi/',
 'https://t5.shwchurch.org/2023/06/24/beijingshouwangjiaohuizhuribaogaoshixiang2023nian6yue25ri/',
 'https://t5.shwchurch.org/2023/06/24/beijingshouwangjiaohui2023nian6yue25rizhurijingbaichengxu/',
-'https://t5.shwchurch.org/2023/06/24/doulaifengxian2023nian6yue25rizhurijiangzhang-xiaofengmushi/',
-'https://t5.shwchurch.org/2023/06/17/beijingshouwangjiaohuizhuribaogaoshixiang2023nian6yue18ri/',
-'https://t5.shwchurch.org/2023/06/17/beijingshouwangjiaohui2023nian6yue18rizhurijingbaichengxu/',
 
 ];
 
